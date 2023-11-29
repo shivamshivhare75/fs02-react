2e858a624b1e901f96c14c3eb2be69bcaf64367c
Shivam Shivhare <100930969+shivamshivhare75@users.noreply.github.com>
2023-11-30 03:53:24 +0530

    Delete Day_16_Homework directory

--- Day_16_Homework/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer(props) {
-    return (
-        <div className="footer">
-            <p className="text-center">Copyright@2021 | All Rights Reserved | by {props.creatername}</p>
-        </div>
-    )
-}
--- Day_16_Homework/src/module2.mjs
+++
@@ -1,7 +0,0 @@
-const a = "Shubham";
-const b = "harry";
-const c = "Rohit";
-const d = "Pankaj";
-
-export default a;
-export {b,c,d};
